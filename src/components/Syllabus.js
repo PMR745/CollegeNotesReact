2acd39bfc354db793a103e5906213e93b5c8eb3d
pmr745 <prathameshrajput7777@gmail.com>
2023-08-06 19:35:54 +0530

    update build

--- src/components/Syllabus.js
+++ src/components/Syllabus.js
@@ -14,14 +14,16 @@ export const Syllabus = () => {
     }, [parentRef])
 
     return (
-        <div className='container' ref={parentRef}>
-            <h1 className="dept">T.Y B-Tech Comp</h1>
-            <h2 className="dept" >Syllabus</h2>
-            <Accordian subjectName="DBMS" page={pageNo} />
-            <Accordian subjectName="Web Development" page={pageNo} />
-            <Accordian subjectName="SEPM" page={pageNo} />
-            <Accordian subjectName="Cloud Computing" page={pageNo} />
-            <Accordian subjectName="Compiler Design" page={pageNo} />
+        <div className="wrapper">
+            <div className='container' ref={parentRef}>
+                <h1 className="dept">T.Y B-Tech Comp</h1>
+                <h2 className="dept" >Syllabus</h2>
+                <Accordian subjectName="DBMS" page={pageNo} />
+                <Accordian subjectName="Web Development" page={pageNo} />
+                <Accordian subjectName="SEPM" page={pageNo} />
+                <Accordian subjectName="Cloud Computing" page={pageNo} />
+                <Accordian subjectName="Compiler Design" page={pageNo} />
+            </div>
         </div>
     )
 }
