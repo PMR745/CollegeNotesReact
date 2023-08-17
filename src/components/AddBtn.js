fd83cef9b8d9eac593a47f9dff1771ca3e51aa08
pmr745 <prathameshrajput7777@gmail.com>
2023-08-17 17:34:42 +0530

    update animations

--- src/components/AddBtn.js
+++ src/components/AddBtn.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 export const AddBtn = (props) => {
 
